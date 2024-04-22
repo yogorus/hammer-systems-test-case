79ba2879e1fea8cd87e454e295712a3684789839
yogorus <yogorus@gmail.com>
2024-04-22 17:48:07 +0300

    move django secret key to env, add readme

--- test_task/test_task/settings.py
+++ test_task/test_task/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-55cw_f76+epm6lde3o-dqbk43!5*=+k4@(d#eso6gd4c&i&$xh'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
